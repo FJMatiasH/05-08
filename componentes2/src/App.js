5ddd4140d71cd31f9d7da3243a32c092a9d75cdd
FJMatiasH <FJMatiasH@uxcale.com>
2024-08-05 19:09:50 +0200

    he cambiado mucho

--- componentes2/src/App.js
+++ componentes2/src/App.js
@@ -3,23 +3,45 @@ import { useState } from 'react';
 
 function App() {
 
-const [left, setLeft] = useState(0)
-const [right, setRight] = useState(0)
+//const [left, setLeft] = useState(0)
+//const [right, setRight] = useState(0)
+
+const [counters, setCounters] = useState({
+  left : 0,
+  right : 0,
+  n : 0,
+  mensaje: 'Mensaje en el estado'
+})
+
+
+const handleClickLeft = () =>{
+  setCounters({
+    ...counters,//trae todo lo que tenemos en counters
+    left: counters.left+1,
+    n: counters.n+1
+  })
+}
+
+const handleClickRight = () =>{
+  setCounters({
+    ...counters,//trae todo lo que tenemos en counters(...)
+    right: counters.right+1,
+    n: counters.n+1
+  })
+}
 
   return (
     <div>
-      
-      {left}
-      <button onClick={() => setLeft(left+1)}>
-      left
-      </button>
 
+      {counters.left}
+      <button onClick={handleClickLeft}>left</button>
 
-      <button onClick={() => setRight(right+1)}>
-      right
-      </button>
-      {right}
+      <button onClick={handleClickRight}>right</button>
+      {counters.right}
       
+      <p>{counters.n}</p>
+
+      <p>{counters.mensaje}</p>
     </div>
   );
 }
